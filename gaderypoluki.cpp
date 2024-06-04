9f8b34c54994d8c11570d8d831088ae5a1be051e
Maciej-Wojcik-Tech <136369949+Maciej-Wojcik-Tech@users.noreply.github.com>
2024-06-04 10:35:47 +0200

    wersja2
    wersja2

--- gaderypoluki.cpp
+++ gaderypoluki.cpp
@@ -1,19 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string szyfr(string old, string res, string pattern)
+string szyfr(string old, string pattern)
 {
+	string res="";
 	for(int i=0;i<old.size();i++)
 	{
 		for(int j=0;j<=pattern.size()-1;j=j+2)
 		{
 			if(old[i]==pattern[j])
 			{
-				res[i]+=pattern[j+1];
+				res=res+pattern[j+1];
 			}
 			else if(old[i]==pattern[j+1])
 			{
-				res[i]+=pattern[j];
+				res+=pattern[j];
+			}
+			else
+			{
+			res +=	old[i];
 			}
 		}
 	}
@@ -24,5 +29,5 @@ main()
 	string res="", old="", pattern="gaderypoluki";
 	cout<<"podaj wyraz do zaszyfrowania"<<endl;
 cin>>old;
-cout<<"zaszyfrowany wyraz to: "<<szyfr(old, res, pattern)<<endl;
+cout<<"zaszyfrowany wyraz to: "<<szyfr(old, pattern)<<endl;
 }
